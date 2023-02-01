47c6a93c0b455c913b03534f1a32df01fb48f8ef
enzo-radel-signo <enzo.radel@signoweb.com.br>
2023-02-01 17:38:58 -0300

    feat: create layout

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import express from "express";
 const app = express();
+
+app.set('view engine', 'ejs');
+app.set("views", __dirname+'/views')
 app.use(express.json())
 
 app.get("/", (request, response) => {
-    
-    return response.json({
-        "chave": "valor"
-    })
+    return response.render("home");
 });
 
 app.listen(3000, () => console.log("iniciou servidor"));
